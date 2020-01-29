d32385f99dab5c98a49e655b0ffd682787f44e29
jaymark <jaykmark@gmail.com>
2020-01-28 21:38:15 -0800

    Start building and styling hero section

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
-import Home from '../index.html'
-import Styles from './scss/app.scss'
-import 'bootstrap'
+import Home from "../index.html";
+import Styles from "./scss/app.scss";
+// import Assets from "../assets";
+import "bootstrap";
 
-console.log("working")
+console.log("working");
--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,65 @@
-const path = require('path');
-const webpack = require('webpack');
+const path = require("path");
+const webpack = require("webpack");
 
 module.exports = {
-	entry: './src/app.js',
+  entry: "./src/app.js",
 
-	output: {
-		path: path.resolve(__dirname, 'dist'),
-		filename: 'bundle.js'
-	},
-	watch: true,
-	devServer: {
-		inline: true
-	},
-	//Add sass-loader
-	module: {
-		rules: [
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js"
+  },
+  watch: true,
+  devServer: {
+    inline: true
+  },
+  //Add sass-loader
+  module: {
+    rules: [
       {
         test: /\.html$/,
         loader: "html-loader"
       },
-			{
-				test: /\.(scss)$/,
-				use: [{
-					loader: 'style-loader'
-				}, {
-					loader: 'css-loader'
-				}, {
-					loader: 'sass-loader'
-				}, {
-					loader: 'postcss-loader',
-					options: {
-						plugins: function () {
-							return [ require('autoprefixer')]
-						}
-					}
-				}]
-			}
-		]
-	}
-}
+      // {
+      //   test: /\.(jpe?g|png|gif|woff|woff2|eot|ttf|svg)(\?[a-z0-9=.]+)?$/,
+      //   loader: "url-loader?limit=100000",
+      //   options: {
+      //     esModule: false
+      //   }
+      // },
+      // {
+      //   test: /\.(jpg|jpeg|gif|png|woff|woff2)$/,
+      //   use: [
+      //     {
+      //       loader: "file-loader",
+      //       options: {
+      //         name: "[path][name].[ext]",
+      //         esModule: false
+      //       }
+      //     }
+      //   ]
+      // },
+      {
+        test: /\.(scss)$/,
+        use: [
+          {
+            loader: "style-loader"
+          },
+          {
+            loader: "css-loader"
+          },
+          {
+            loader: "sass-loader"
+          },
+          {
+            loader: "postcss-loader",
+            options: {
+              plugins: function() {
+                return [require("autoprefixer")];
+              }
+            }
+          }
+        ]
+      }
+    ]
+  }
+};
