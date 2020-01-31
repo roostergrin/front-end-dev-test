4fe15a5322fbfb460016f7f18b7d6e0da6a95054
jaymark <jaykmark@gmail.com>
2020-01-30 19:29:43 -0800

    Finalize major styling

--- src/app.js
+++ src/app.js
@@ -2,12 +2,12 @@ import Home from "../index.html";
 import Styles from "./scss/app.scss";
 import "bootstrap";
 
+// Slick Carousel w/ jQuery
 $(document).ready(() => {
   $('.carousel .slick').slick({
     dots: true,
     arrows: true,
   });
 });
-
 
 console.log("working");
